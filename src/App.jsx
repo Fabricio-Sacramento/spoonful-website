05fbc9a0f920df84daa2b741e44b488d16a4098c
Fabricio-Sacramento <fabriciomsacramento@gmail.com>
2025-02-06 19:23:25 -0300

    Limpeza do projeto. Exclusão de arquivos sem uso

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
-import Background from './components/Background';
 import DynamicBackground from './components/DynamicBackground';
 import Glass from './components/Glass';
-import './styles/Background.css'; // Garante que o CSS do background está carregado
 
 const App = () => {
   return (
     <>
-      {/* O Background é renderizado fora do Canvas para que fique como camada de fundo */}
-      <Background />
+      {/* O Background é renderizado fora do Canvas para que fique como camada de fundo 
+      <Background /> */}
       
       <Canvas 
         camera={{ position: [0, 1, 5], fov: 50 }}
@@ -25,9 +23,6 @@ const App = () => {
         
         {/* Luz pontual para realçar brilhos */}
         <pointLight position={[-5, 5, 5]} intensity={1} />
-
-        {/* Background 3D dentro do Canvas
-        <EnvBackground /> */}
 
         {/* Background dinâmico dentro do Canvas */}
         <DynamicBackground />
