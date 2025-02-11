c1c4ecb2e20af11e0cd215f97b5065db9890fada
Fabricio-Sacramento <fabriciomsacramento@gmail.com>
2025-02-10 22:32:51 -0300

    Removida tentativa anterior de simulação física da água
    - Excluído `WaterPhysics.jsx` e removida a física do projeto
    - Removida importação e uso de `Physics` em `App.jsx`
    - Corrigido `GlassCube.jsx` para remover referência à física
    - Preparação para implementação da simulação de água com partículas

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,8 @@ const App = () => {
         <ambientLight intensity={1} />
         <directionalLight position={[5, 5, 5]} intensity={2} castShadow />
         <pointLight position={[-5, 5, 5]} intensity={1} />
-
         <DynamicBackground />
-        <GlassCube position={[0, 0, 0]} />
+        <GlassCube /> 
         <Glass />
         <OrbitControls />
       </Canvas>
