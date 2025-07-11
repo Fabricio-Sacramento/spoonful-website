fa14ade677f67fe022632e2cde143e2359fb0401
Fabricio-Sacramento <fabriciomsacramento@gmail.com>
2025-07-11 19:23:45 -0300

    Ajuste import WhaWeDo em main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import WhatWeDo from './components/WhatWeDo';
+import WhatWeDo from './components/WhatWeDo/WhatWeDo';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
