0c26f8987f1a8fbf31c8e257fb559801d1605ab1
Fabricio-Sacramento <fabriciomsacramento@gmail.com>
2025-07-29 17:15:10 -0300

    cleanup

--- src/components/WorkIntro.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/WorkIntro.jsx
-
-export default function WorkIntro() {
-  return (
-    <div className="work-slide work-intro">
-      <h1 className="work-intro-title">WORK</h1>
-    </div>
-  );
-}
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import WorkSection from './components/WorkSection.jsx';
 
 // 1) Monta o canvas 3D / cena React no #root
 const root3D = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,13 +11,4 @@ root3D.render(
   </React.StrictMode>
 );
 
-// 2) Monta o WorkSection no container estático #work-root
-const workRootEl = document.getElementById('work-root');
-if (workRootEl) {
-  const workRoot = ReactDOM.createRoot(workRootEl);
-  workRoot.render(
-    <React.StrictMode>
-      <WorkSection />
-    </React.StrictMode>
-  );
-}
+
