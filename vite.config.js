dc0c31bfa7f5dcabb6537ebd3456eb2477887d1b
Fabricio-Sacramento <fabriciomsacramento@gmail.com>
2025-11-25 12:12:55 -0300

    fix: simplifica config Vite - remove CSS plugin

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import svgr from 'vite-plugin-svgr';
+import svgr from 'vite-plugin-svgr'
 import glsl from 'vite-plugin-glsl'
-import devtoolsJson from 'vite-plugin-devtools-json'
-import cssExtract from 'rollup-plugin-css-only'
 
 export default defineConfig({
   plugins: [
-    devtoolsJson(),                                 // só no dev e build
     react(),
     svgr(),
-    glsl(),
-    cssExtract({
-      output: 'styles/style.css',
-      apply: 'build'                                // ← somente em build
-    })
-  ],
-  css: {
-    devSourcemap: true
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: 'styles/[name][extname]'    // mantém /styles/style.css em prod
-      }
-    }
-  }
+    glsl()
+  ]
 })
