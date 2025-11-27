9ef5d1faa3272ba8c42c99924701b14e995e98b5
Fabricio-Sacramento <fabriciomsacramento@gmail.com>
2025-11-27 16:48:40 -0300

    primeira abordagem is=mobile e touch action não funcionou

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import { Canvas, useThree } from '@react-three/fiber';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Glass from './components/Glass';
 import DynamicBackground from './components/DynamicBackground';
 import GlassCube from './components/GlassCube';
@@ -12,37 +12,43 @@ import { OrbitControls } from '@react-three/drei';
 // ===================================================
 function CanvasReadySignal() {
   const { gl } = useThree();
-
   useEffect(() => {
     // garante que o frame inicial foi renderizado
     requestAnimationFrame(() => {
       window.dispatchEvent(new Event('canvas:ready'));
     });
   }, [gl]);
-
   return null;
 }
 
 const App = () => {
-  return (
-    <Canvas frameloop="demand" camera={{ position: [0, 0, 5], fov: 50 }} shadows>
-      <AppCanvasCleanupRegistrar />
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 1023);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 1023);
+    };
+    
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return (
+    <Canvas frameloop="demand" camera={{ position: [0, 0, 5], fov: 50 }} shadows style={{ touchAction: isMobile ? 'pan-y' : 'none' }}>
+      <AppCanvasCleanupRegistrar />
       <ambientLight intensity={1} />
       <directionalLight position={[5, 5, 5]} intensity={2} castShadow />
-
       <DynamicBackground />
       <Glass />
       <GlassCube />
-
       <OrbitControls
+        enabled={!isMobile}
         enableZoom={false}
         enablePan={false}
         enableRotate={true}
         enableDamping={true}
         dampingFactor={0.05}
       />
-
       {/* Adiciona o emissor do evento de readiness */}
       <CanvasReadySignal />
     </Canvas>
